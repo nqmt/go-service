9784a02560c0a64d00899feed9d043291d8874e9
nqmt <to.nqmt@gmail.com>
2019-07-24 14:30:37 +0700

    reformat code

--- domain/soap/entity.go
+++ domain/soap/entity.go
@@ -1,9 +1,9 @@
 package soap
 
 type Soap struct {
-	PersonID string `db:"PERSONID"`
-	LastName string `db:"LASTNAME"`
+	PersonID  string `db:"PERSONID"`
+	LastName  string `db:"LASTNAME"`
 	FirstName string `db:"FIRSTNAME"`
-	Address string `db:"ADDRESS"`
-	City string `db:"CITY"`
+	Address   string `db:"ADDRESS"`
+	City      string `db:"CITY"`
 }
